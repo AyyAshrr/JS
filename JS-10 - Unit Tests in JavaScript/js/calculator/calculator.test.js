a7b9402a74a7fcc520176cbe167053e42f7399a7
Ashleigh <ashleigh@Ashleighs-MacBook-Pro.local>
2020-06-02 12:48:44 +0100

    part 2 has been completed and tested successfully

--- JS-10 - Unit Tests in JavaScript/js/calculator/calculator.test.js
+++ JS-10 - Unit Tests in JavaScript/js/calculator/calculator.test.js
@@ -15,3 +15,21 @@ test('subtract 5 - 4 to equal 1', () => {
 test('subtract -5 - -10 to equal 5', () => {
   expect(calculator.subtract(-5, -10)).toBe(5);
 });
+
+//Implement 2 unit tests for the *divide* function.
+test('divide 10 / by 2 to equal 5', () => {
+  expect(calculator.divide(10, 2)).toBe(5);
+});
+
+test('divide 34874 / by 32 to equal 1089', () => {
+  expect(calculator.divide(34874, 32)).toBe(1089.8125);
+});
+
+//Implement 2 unit tests for the *multiply* function.
+test('multiply 2468 * 78 to equal 192504', () => {
+  expect(calculator.multiply(2468, 78)).toBe(192504);
+});
+
+test('multiply 12 * 12 to equal 144', () => {
+  expect(calculator.multiply(12, 12)).toBe(144);
+});
